f388ff1b5de92e92e6003e3fa2602ce6ea90417d
minkyngkm <minkyngkm@gmail.com>
2021-03-07 14:59:49 +0900

    create&delete mutations

--- graphql/db.js
+++ graphql/db.js
@@ -1,38 +1,55 @@
-export const people = [
-  { id: 1, name: "Min", age: 15, gender: "female" },
+export let movies = [
+  { id: 1, name: "Min", score: 15, gender: "female" },
   {
     id: 2,
     name: "Apple",
-    age: 12,
-    gender: "female",
+    score: 12,
   },
   {
     id: 3,
     name: "Banana",
-    age: 10,
-    gender: "female",
+    score: 10,
   },
   {
     id: 4,
     name: "Cake",
-    age: 15,
-    gender: "male",
+    score: 15,
   },
   {
     id: 5,
     name: "Deer",
-    age: 25,
-    gender: "male",
+    score: 25,
   },
   {
     id: 6,
     name: "Fish",
-    age: 13,
-    gender: "female",
+    score: 13,
   },
 ];
 
+export const getMovies = () => movies;
+
 export const getById = (id) => {
-  const filteredPeople = people.filter((person) => person.id === id);
-  return filteredPeople[0];
+  const filteredMovie = movies.filter((movie) => movie.id === id);
+  return filteredMovie[0];
+};
+
+export const deleteMovie = (id) => {
+  const cleanedMovies = movies.filter((movie) => movie.id !== id);
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: `${movies.length + 1}`,
+    name,
+    score,
+  };
+  movies.push(newMovie);
+  return newMovie;
 };
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,14 +1,22 @@
 // resolve query
-import { people, getById } from "./db";
+import { getById, addMovie, getMovies, deleteMovie } from "./db";
 const resolvers = {
   Query: {
-    people: () => people,
-    person: (_, { id }) => {
+    movies: () => getMovies(),
+    movie: (_, { id }) => {
       return getById(id);
     },
+  },
+
+  Mutation: {
+    addMovie: (_, { name, score }) => addMovie(name, score),
+    deleteMovie: (_, { id }) => deleteMovie(id),
   },
 };
 
 export default resolvers;
 
 // instead of writing 'root' or 'source' I write _
+
+// query is used for SELECT ( GET in REST apis )
+// mutation is used for INSERT/UPDATE/DELETE ( POST, PUT, PATCH, DELETE in REST apis)
